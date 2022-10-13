ff664855a07377a36c7f07e7e2770bcb64d326b4
benJamin <bzawodni@gmail.com>
2022-10-12 20:20:30 -0400

    downloading prereq.sh

--- host-config.sh
+++ host-config.sh
@@ -2,17 +2,35 @@
 
 echo "Securing your enviroment"
 
-pkg_opts="-y"
-echo "Using apt to prepare packages for ${DISTRO} system"
-echo "  Updating system packages..."
-$sudo apt-get ${pkg_opts} install curl > /dev/null
-$sudo apt-get ${pkg_opts} update > /dev/null
-echo "  Installing missing prerequisite packages, if any.."
-pkg_list="libpq-dev python3 build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev systemd libsystemd-dev libsodium-dev zlib1g-dev make g++ tmux git jq libncursesw5 gnupg aptitude libtool autoconf secure-delete iproute2 bc tcptraceroute dialog automake sqlite3 bsdmainutils libusb-1.0-0-dev libudev-dev unzip"
-$sudo apt-get ${pkg_opts} install ${pkg_list} > /dev/null;rc=$?
-if [ $rc != 0 ]; then
-  echo "An error occurred while installing the prerequisite packages, please investigate by using the command below:"
-  echo "$sudo apt-get ${pkg_opts} install ${pkg_list}"
-  echo "It would be best if you could submit an issue at ${REPO} with the details to tackle in future, as some errors may be due to external/already present dependencies"
-  err_exit
+###################################
+# sudo password for sudo commands #
+###################################
+
+if [[ "$(/usr/bin/whoami)" != "root" ]]; then
+sudo -p "The script needs the admin/sudo password to continue, please enter: " date 2>/dev/null 1>&2
+        if [ ! $? = 0 ]; then
+            echo "You entered an invalid password. Script aborted."
+            exit 1
+        fi
 fi
+
+####################################
+# Operating System (Linux) upgrade #
+####################################
+read -p "Update Operating System (Linux)? (yes or [no]): " INPUT
+
+case $INPUT in
+  y|yes)
+        echo "Updating Operating System (Linux)... please wait"
+        sleep 3
+        sudo apt-get update        # command is used to download package information from all configured sources.
+        sudo apt-get upgrade       # You run sudo apt-get upgrade to install available upgrades of all packages currently installed on the system from the sources configured via sources. list file. New packages will be installed if required to satisfy dependencies, but existing packages will never be removed
+        ;;
+*)
+        echo "Skipped! The software upgrade will continue without updating the Operating System... please wait"
+        sleep 3
+        ;;
+esac
+
+wget wget https://raw.githubusercontent.com/cardano-community/guild-operators/alpha/scripts/cnode-helper-scripts/prereqs.sh
+chmod +x prereqs.sh
