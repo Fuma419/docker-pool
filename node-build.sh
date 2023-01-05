e29ac782309d86f0f9f6c0e13fe923bf878bcffc
benJamin <bzawodni@gmail.com>
2023-01-04 20:43:23 -0500

    adding submit api support

--- node-build.sh
+++ node-build.sh
@@ -113,12 +113,13 @@ docker run -dit \
 -e CPU_CORES=2 \
 -p 3000:6000 \
 -p 12799:12798 \
+-p 8091:8090 \
 -v /opt/cardano/$NODE_NAME/db:/opt/cardano/cnode/db \
 -v /opt/cardano/$NODE_NAME/files:/opt/cardano/cnode/files \
--v /opt/cardano/$NODE_NAME/scripts/cnode.sh:/opt/cardano/cnode/scripts/cnode.sh \
 cardanocommunity/cardano-node
 EOF
 
+cp cfg/entrypoint.sh.submit.8091 /entrypoint.sh
 cp cfg/$NETWORK-topology.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-topology.json
 cp cfg/$NETWORK-config.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-config.json
 
@@ -143,10 +144,10 @@ docker run -dit \
 -p 8090:8090 \
 -v /opt/cardano/$NODE_NAME/db:/opt/cardano/cnode/db \
 -v /opt/cardano/$NODE_NAME/files:/opt/cardano/cnode/files \
--v /opt/cardano/$NODE_NAME/scripts/submitapi.sh:/opt/cardano/cnode/scripts/submitapi.sh \
 cardanocommunity/cardano-node
 EOF
 
+cp cfg/entrypoint.sh.submit.8090 /entrypoint.sh
 cp cfg/$NETWORK-topology.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-topology.json
 cp cfg/$NETWORK-config.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-config.json
 
